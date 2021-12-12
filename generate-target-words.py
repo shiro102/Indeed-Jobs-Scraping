1450776a98437c84ec815a3d1eaf386d886cecd6
Khai Hung <hungluongkhai@gmail.com>
2021-12-12 14:32:48 -0800

    Update comment in generate-target-words.py.

--- generate-target-words.py
+++ generate-target-words.py
@@ -1,6 +1,7 @@
 import json
 
-# This file generate the list of targeted words that can be the signals to find qualification in a job posting #
+# This file generate the list of targeted words that can be the signals to find qualification section (or required skills) in a job posting #
+
 list_of_words = ['Requirement', 'Required', 'Skill', 'Knowledge', 'Qualification', 'Qualified', 'Preference',
                  'Preferred', 'Experience', 'Need', 'Needed', 'Expected', 'Looking for', 'Must have']
 
